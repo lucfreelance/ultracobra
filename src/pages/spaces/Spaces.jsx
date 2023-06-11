5ad66bfa7f6d802151e7152c2d8b7dc50bfdaf10
sslluuccaa <soelucas3@gmail.com>
2023-06-11 12:09:26 -0500

    Implementing a solution to resolve the routing issues in the webapp. I still don't know if works, will see

--- src/pages/spaces/Spaces.jsx
+++ src/pages/spaces/Spaces.jsx
@@ -1,13 +1,17 @@
 
 import Blog3 from '../spaces/blog3/Blog-3';
+// import Blog3 from '../spaces/blog2/Blog-2';
 import Blog2 from './Blog-2';
+// import Blog3 from '../spaces/blog1/Blog-1';
 import Blog1 from './Blog-1';
 
 const Spaces = () => {
   return (
     <>
       <div>
-      <Blog3 /> <br /> <br /> <Blog2 /> <br /> <br /> <Blog1 />
+      <Blog3 /> <br /> <br />
+      <Blog2 /> <br /> <br />
+      <Blog1 />
       </div>
     </>
   );
