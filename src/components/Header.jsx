0008d50a23ead759c3d135b3d5c50b6bc121affc
sslluuccaa <soelucas3@gmail.com>
2023-06-11 23:43:25 -0500

    Updating improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,47 @@
-
-import UltraHead from '../img/ultra-head-square.png';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import UltraHead1 from '../img/ultra-head-square.png';
+import UltraHead2 from '../img/ultra-head-square-alt.png';
 import { Link } from 'react-router-dom';
-import TarjetaFront from "./TarjetaFront";
+import TarjetaFront from './TarjetaFront';
 import '../App.css';
 
 const Header = () => {
+  const [isTransitioned, setIsTransitioned] = useState(false);
+
+  const toggleTransition = () => {
+    setIsTransitioned(!isTransitioned);
+  };
+
+  const transitionVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
   return (
     <header>
       <h1>
-      <Link to="/Pitch"><span className="ultra">Ⓤ</span> ultracobra{' '}
-        <span className="emoji">🐍</span></Link>
-      </h1>          
-    <TarjetaFront />
-            <div>
-            <img
-              src={UltraHead}
-              alt="Relax and contact ultracobra services"
-              className="ultra-head"
-            />
-    <hr className="divisory-line" />
-    <p>Ultracobra is where you have some fun and a space break</p>
-    </div>
-    </header> 
+        <Link to="/Pitch">
+          <span className="ultra">Ⓤ</span> ultracobra{' '}
+          <span className="emoji">🐍</span>
+        </Link>
+      </h1>
+      <TarjetaFront />
+      <div>
+        <motion.img
+          src={isTransitioned ? UltraHead2 : UltraHead1}
+          alt="Relax and contact ultracobra services"
+          className={`ultra-head ${isTransitioned ? 'transitioned' : ''}`}
+          onClick={toggleTransition}
+          initial="initial"
+          animate="animate"
+          variants={transitionVariants}
+          transition={{ duration: isTransitioned ? 2 : 4 }}
+        />
+        <hr className="divisory-line" />
+        <p>Ultracobra is where you have some fun and a space break</p>
+      </div>
+    </header>
   );
 };
 
