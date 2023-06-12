88e3184c57f1ad64e805d35f376c43a886c113f1
sslluuccaa <soelucas3@gmail.com>
2023-06-12 01:11:33 -0500

    Improving Footer, Space 1 and other details

--- src/pages/spaces/Blog-1.jsx
+++ src/pages/spaces/Blog-1.jsx
@@ -1,6 +1,79 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
 import Blog2Top from './assets/block-2.png';
 import '../../custom.css';
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const CardHeader = styled.header`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const CardImage = styled.img`
+  width: 200px;
+`;
+
+const CardMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const CardInput = styled.input`
+  margin-bottom: 10px;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  width: 300px;
+`;
+
+const ResultButton = styled.button`
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #4caf50;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const ResultMessage = styled.p`
+  margin-top: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 16px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const StartOverButton = styled.button`
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #f44336;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const Title = styled.h2`
+  background-color: #2196f3;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
 
 const Blog1 = () => {
   const [input1, setInput1] = useState('');
@@ -48,79 +121,75 @@ const Blog1 = () => {
   };
 
   return (
-    <div className="blog2-container">
-        <h2>Crystal Ball Story</h2>
-      <h3>Answer questions to the Ball and receive a fun interactive story based on your answers:</h3>
-      <header>
-        <div className="text-center">
-          <img className="ball" src="https://i.imgur.com/V9J9qBt.png" alt="Crystal Ball - Comic" />
-        </div>
-      </header>
-      <main>
-        <input
+    <CardContainer>
+      <Title>Crystal Ball Story</Title>
+      <CardHeader>
+        <CardImage src="https://i.imgur.com/V9J9qBt.png" title="Answer questions to the Ball and receive a fun interactive story based on your answers" alt="Answer questions to the Ball and receive a fun interactive story based on your answers" />
+      </CardHeader>
+      <CardMain>
+        <CardInput
           id="trozo1"
           placeholder="🎭 Your favorite actor / actress"
           value={input1}
           onChange={(e) => setInput1(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo2"
           placeholder="🌠 Best vacation destination"
           value={input2}
           onChange={(e) => setInput2(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo3"
           placeholder="🧗‍♂️ Your favorite hobby"
           value={input3}
           onChange={(e) => setInput3(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo4"
-          placeholder="🤬 Famous person who annoys you that most"
+          placeholder="🤬 Famous person who annoys you more"
           value={input4}
           onChange={(e) => setInput4(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo5"
           placeholder="😱 Your deepest fear [If possible to say]"
           value={input5}
           onChange={(e) => setInput5(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo6"
-          placeholder="😭 Most intense event ever ⬆️ Same thing"
+          placeholder="😭 Most intense event ever ⬆️ Same"
           value={input6}
           onChange={(e) => setInput6(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo7"
           placeholder="🥳 Favorite Saturday activity"
           value={input7}
           onChange={(e) => setInput7(e.target.value)}
-        /><br />
-        <input
+        />
+        <CardInput
           id="trozo8"
           placeholder="😎 Favorite Sunday activity"
           value={input8}
           onChange={(e) => setInput8(e.target.value)}
-        /><br />
-        <p className="resultado">
-          <button id="btnCalcular" onClick={btnOnClick}>
-          🔥 Press the button 🔥 for the story❗
-          </button>
+        />
+        <p className="resultado" >
+          <ResultButton id="btnCalcular" onClick={btnOnClick}>
+            🔥 Press the button 🔥 for the story❗
+          </ResultButton>
         </p>
         {result && (
-          <p id="result" className="result-message">
+          <ResultMessage id="result" className="result-message">
             {result}
-          </p>
+          </ResultMessage>
         )}
-      </main>
-      <button onClick={resetInputs}>Start over...</button>
-        <span className="oculto">
-        </span>
-    </div>
+      </CardMain>
+      <StartOverButton onClick={resetInputs}>Start over...</StartOverButton>
+      <span className="oculto"></span>
+    </CardContainer>
   );
 };
-<br />
+
 export default Blog1;
